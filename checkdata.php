3e67e0f82b8c3aa41feed08ded0c25cac85740a9
Francesco Stucci <FranceStu96@gmail.com>
2018-04-20 14:21:09 +0200

    Aggiunti file php per generare codice fiscale e verificare il login

---
+++ checkdata.php
@@ -0,0 +1,34 @@
+<?php
+  require "utilities.php";
+  const MALE = "male";
+  const FEMALE = "female";
+
+  $required = array('name', 'surname', 'gender', 'birthDate', 'birthCity');
+
+  foreach($required as $field) {
+    if (empty($_POST[$field])) {
+      dontHackMySite();
+      return;
+    }
+  }
+
+  if($_POST['gender'] != MALE && $_POST['gender'] != FEMALE){
+    dontHackMySite();
+    return;
+  }
+
+  $name=$_POST['name'];
+  $birthCity=$_POST['birthCity'];
+  $surname=$_POST['surname'];
+  $gender=$_POST['gender'];
+  $birthDate=strtotime($_POST['birthDate']);
+
+  $fiscalCode = getFiscalCode($name, $surname, $gender == MALE ? 0 : 40, $birthDate, $birthCity);
+
+  if (empty($fiscalCode))
+    dontHackMySite();
+  else{
+    echo "<center><h2>Your fiscal code is: $fiscalCode </h2>";
+    echo "<input type=\"button\" value=\"Home\" onclick=\"document.location.href='index.html'\"></center>";
+  }
+?>
